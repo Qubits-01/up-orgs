7b20b731347ecfb96609111e44da58421a0e369b
Marc Peejay Viernes <mpvviernes@gmail.com>
2023-02-13 08:36:40 +0800

    Add UpCampusesEnum (w/ test)

---
+++ lib/constants/enums/up_campuses_enum.dart
@@ -0,0 +1,13 @@
+enum UpCampuses {
+  diliman('Diliman'),
+  losBanos('Los Baños'),
+  manila('Manila'),
+  visayas('Visayas'),
+  openUniversity('Open University'),
+  mindanao('Mindanao'),
+  baguio('Baguio'),
+  cebu('Cebu');
+
+  const UpCampuses(this.name);
+  final String name;
+}
