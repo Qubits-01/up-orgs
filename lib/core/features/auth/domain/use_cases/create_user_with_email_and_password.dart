2f981aa6eb73eba05d8aa19697af005994b2435e
Marc Peejay Viernes <mpvviernes@gmail.com>
2023-02-18 09:58:12 +0800

    Add CreateUserWithEmailAndPassword use case

--- lib/core/features/auth/domain/use_cases/create_user_with_email_and_password.dart
+++ lib/core/features/auth/domain/use_cases/create_user_with_email_and_password.dart
@@ -1,3 +1,7 @@
+import 'package:fpdart/fpdart.dart';
+import '../../../../failures/auth_failure.dart';
+import '../../data/repo_impl/auth_repo_impl.dart';
+
 import '../../../../interfaces/params_intf.dart';
 import '../../../../interfaces/use_case_intf.dart';
 import '../entities/user_entity.dart';
@@ -7,10 +11,18 @@ typedef ThisUseCase = UseCaseIntf<UserEntity, Params>;
 class CreateUserWithEmailAndPassword implements ThisUseCase {
   // TODO: Add test on this class.
 
-  const CreateUserWithEmailAndPassword();
+  const CreateUserWithEmailAndPassword({required AuthRepoImpl authRepo})
+      : _authRepo = authRepo;
+
+  final AuthRepoImpl _authRepo;
 
   @override
-  call(Params params) {}
+  Future<Either<AuthFailure, UserEntity>> call(Params params) async {
+    return await _authRepo.createUserWithEmailAndPasswordUseCase(
+      email: params.email,
+      password: params.password,
+    );
+  }
 }
 
 class Params implements ParamsIntf {
@@ -21,6 +33,9 @@ class Params implements ParamsIntf {
         _password = password;
 
   final String _email, _password;
+
+  String get email => _email;
+  String get password => _password;
 
   @override
   List<Object?> get props => [_email, _password];
